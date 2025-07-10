eb93cc00e8223030f76db4383af0f194902058d1
solomon-211 <s.leek@alustudent.com>
2025-07-10 16:05:48 -0700

    1-square.py

--- python-classes/1-square.py
+++ python-classes/1-square.py
@@ -1,4 +1,14 @@
 #!/usr/bin/python3
+"""Square module"""
+
+
 class Square:
+    """A square class"""
+    
     def __init__(self, size):
+        """Initialize a square with given size
+        
+        Args:
+            size: The size of the square
+        """
         self.__size = size
