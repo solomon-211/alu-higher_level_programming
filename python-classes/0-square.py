d24c47216a08ed0f8a32ea43e6acfe5c786d60e2
solomon-211 <s.leek@alustudent.com>
2025-07-10 15:21:00 -0700

    0-square.py

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -1,4 +1,13 @@
 #!/usr/bin/python3
+"""A module containing an empty Square class."""
+
+
 class Square:
     """An empty class that defines a square."""
     pass
+
+
+if __name__ == "__main__":
+    mysquare = Square()
+    print(type(mysquare))
+    print(mysquare.__dict__)
