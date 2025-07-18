114d3efdc9773df3261467f178709b7e0ef530cd
solomon-211 <s.leek@alustudent.com>
2025-07-18 14:29:54 -0700

    8-count_89.sql

---
+++ SQL_introduction/8-count_89.sql
@@ -0,0 +1,4 @@
+-- Script to count records with id = 89 in first_table from hbtn_0c_0 database
+-- Usage: mysql -u username -p hbtn_0c_0 < script_name.sql
+
+SELECT COUNT(*) FROM first_table WHERE id = 89;
