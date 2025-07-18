c6e5a03c4a6fa07029182f7eca15adb30b0d0eb4
solomon-211 <s.leek@alustudent.com>
2025-07-18 14:12:48 -0700

    4-first_table.sql

---
+++ SQL_introduction/4-first_table.sql
@@ -0,0 +1,7 @@
+-- Task: Create table first_table if it doesn't exist
+
+-- Query to create table with specified columns
+CREATE TABLE IF NOT EXISTS first_table (
+    id INT,
+    name VARCHAR(256)
+);
