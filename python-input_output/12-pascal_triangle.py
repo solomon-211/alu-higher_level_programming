a0c5872fa259c307fcde18e66041463dc4aadae9
solomon-211 <s.leek@alustudent.com>
2025-07-18 11:24:02 -0700

    12-pascal_triangle.py

---
+++ python-input_output/12-pascal_triangle.py
@@ -0,0 +1,19 @@
+def pascal_triangle(n):
+    """Returns a list of lists of integers representing Pascal's triangle of n."""
+    if n <= 0:
+        return []
+    
+    triangle = [[1]]
+    
+    for i in range(1, n):
+        prev_row = triangle[-1]
+        new_row = [1]
+        
+        # Generate the middle elements
+        for j in range(1, i):
+            new_row.append(prev_row[j-1] + prev_row[j])
+        
+        new_row.append(1)
+        triangle.append(new_row)
+    
+    return triangle
