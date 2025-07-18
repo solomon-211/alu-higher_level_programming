ff9d8a666e1f4433f279da50394a1c413121d59d
solomon-211 <s.leek@alustudent.com>
2025-07-18 14:28:08 -0700

    7-insert_value.sql

---
+++ SQL_introduction/7-insert_value.sql
@@ -0,0 +1,4 @@
+-- Script to insert a new row in first_table from hbtn_0c_0 database
+-- Usage: mysql -u username -p hbtn_0c_0 < script_name.sql
+
+INSERT INTO first_table (id, name) VALUES (89, 'Best School');
