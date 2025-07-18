9739d41ccdc732af1219178a9cc2f2b8af61a4c0
solomon-211 <s.leek@alustudent.com>
2025-07-18 14:23:27 -0700

    5-full_table.sql

--- SQL_introduction/5-full_table.sql
+++ SQL_introduction/5-full_table.sql
@@ -1,17 +1,4 @@
 -- Script to print full description of first_table from hbtn_0c_0 database
 -- Usage: mysql -u username -p hbtn_0c_0 < script_name.sql
 
-SELECT 
-    COLUMN_NAME,
-    COLUMN_TYPE,
-    IS_NULLABLE,
-    COLUMN_KEY,
-    COLUMN_DEFAULT,
-    EXTRA
-FROM 
-    INFORMATION_SCHEMA.COLUMNS 
-WHERE 
-    TABLE_SCHEMA = 'hbtn_0c_0' 
-    AND TABLE_NAME = 'first_table'
-ORDER BY 
-    ORDINAL_POSITION;
+SHOW CREATE TABLE first_table;
