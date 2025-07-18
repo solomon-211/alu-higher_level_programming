0a0ebbc8be2100a8e0a391ef8e57f703ba277f86
solomon-211 <s.leek@alustudent.com>
2025-07-18 11:47:57 -0700

    10-student.py

--- python-input_output/10-student.py
+++ python-input_output/10-student.py
@@ -16,7 +16,7 @@ class Student:
         if (isinstance(attrs, list) and
                 all(isinstance(attr, str) for attr in attrs)):
             return {
-                    attr: getattr(self, attr) 
+                    attr: getattr(self, attr)
                     for attr in attrs if hasattr(self, attr)
                     }
         return self.__dict__
