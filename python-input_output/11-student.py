e22d5f1c1c3986efca87dcb06829c7d6690e66b1
solomon-211 <s.leek@alustudent.com>
2025-07-18 13:29:20 -0700

    11-student.py

--- python-input_output/11-student.py
+++ python-input_output/11-student.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
-"""Defines a Student class with selective JSON serialization."""
+"""Defines a Student class with serialization/deserialization capabilities."""
 
 
 class Student:
-    """Represents a student with customizable JSON conversion."""
+    """Represents a student with JSON serialization/deserialization."""
 
     def __init__(self, first_name, last_name, age):
         """Initialize a new Student instance."""
@@ -14,7 +14,14 @@ class Student:
     def to_json(self, attrs=None):
         """Retrieve a dictionary representation of the Student instance."""
         if (isinstance(attrs, list) and
-                all(isinstance(attr, str) for attr in attrs)):
-            return {attr: getattr(self, attr)
-                   for attr in attrs if hasattr(self, attr)}
+           all(isinstance(attr, str) for attr in attrs)):
+            return {
+                attr: getattr(self, attr)
+                for attr in attrs if hasattr(self, attr)
+            }
         return self.__dict__
+
+    def reload_from_json(self, json):
+        """Replace all attributes of the Student instance from a dictionary."""
+        for key, value in json.items():
+            setattr(self, key, value)
