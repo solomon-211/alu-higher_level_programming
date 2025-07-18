f6523f73b3118d1827e604ff73064acfbb9d2977
solomon-211 <s.leek@alustudent.com>
2025-07-18 14:49:45 -0700

    0-privileges.sql

---
+++ SQL_more_queries/0-privileges.sql
@@ -0,0 +1,7 @@
+-- Task: List privileges for MySQL users user_0d_1 and user_0d_2
+
+-- Query to show grants for user_0d_1
+SHOW GRANTS FOR 'user_0d_1'@'localhost';
+
+-- Query to show grants for user_0d_2
+SHOW GRANTS FOR 'user_0d_2'@'localhost';
