b08de630b2e380bd581fd75e0938f241fdc10fdf
solomon-211 <s.leek@alustudent.com>
2025-07-18 10:01:58 -0700

    0-read_file.py

---
+++ python-input_output/0-read_file.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+"""Module to read and print a file."""
+
+def read_file(filename=""):
+    """Read a UTF-8 file and print its contents."""
+    with open(filename, encoding="utf-8") as file:
+        print(file.read(), end="")
