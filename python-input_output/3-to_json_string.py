6a6ee48992ef85893a3cb8379c3b8fc25718a3c4
solomon-211 <s.leek@alustudent.com>
2025-07-18 10:21:12 -0700

    3-to_json_string.py

---
+++ python-input_output/3-to_json_string.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""Module for converting Python objects to JSON strings."""
+
+import json
+
+def to_json_string(my_obj):
+    """Return the JSON string representation of a Python object.
+    """
+    return json.dumps(my_obj)
