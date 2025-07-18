4a4f19e3317702ca74451fa85b030cdef1387bb6
solomon-211 <s.leek@alustudent.com>
2025-07-18 11:04:58 -0700

    9-student.py

---
+++ python-input_output/9-student.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""Defines a Student class with JSON serialization capability."""
+
+class Student:
+    """Represents a student with basic information and JSON conversion."""
+    
+    def __init__(self, first_name, last_name, age):
+        """Initialize a new Student instance.
+        """
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+    
+    def to_json(self):
+        """Retrieve a dictionary representation of the Student instance.
+        """
+        return self.__dict__
