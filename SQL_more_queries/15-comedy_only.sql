651011b5b64a1cf3ce9181a86295a656e0a42871
solomon-211 <s.leek@alustudent.com>
2025-07-31 13:42:52 -0700

    15-comedy_only.sql

--- SQL_more_queries/15-comedy_only.sql
+++ SQL_more_queries/15-comedy_only.sql
@@ -1,7 +1,7 @@
--- Lists all Comedy shows in alphabetical order
-SELECT tv_shows.title
-FROM tv_shows
-JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
-JOIN tv_genres ON tv_genres.id = tv_show_genres.genre_id
-WHERE tv_genres.name = 'Comedy'
-ORDER BY tv_shows.title ASC;
+-- Lists all Comedy shows alphabetically
+SELECT s.title
+FROM tv_shows s
+INNER JOIN tv_show_genres sg ON s.id = sg.show_id
+INNER JOIN tv_genres g ON g.id = sg.genre_id
+WHERE g.name = 'Comedy'
+ORDER BY s.title;
