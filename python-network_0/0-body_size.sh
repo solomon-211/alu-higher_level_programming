b909a9609a06b882ac752670c661d9cd5a4bf35e
solomon-211 <s.leek@alustudent.com>
2025-07-31 14:04:01 -0700

    0-body_size.sh

--- python-network_0/0-body_size.sh
+++ python-network_0/0-body_size.sh
@@ -1,5 +1,3 @@
 #!/bin/bash
-# Sends a request to a URL and displays the size of the response body in bytes
-
-# Use curl to fetch the URL and count the bytes
-curl -sI "$1" | grep -i Content-Length | awk '{print $2}'
+# Displays the size in bytes of the response body from a URL request
+curl -s "$1" | wc -c
