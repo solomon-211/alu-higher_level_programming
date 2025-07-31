493b66aa84ebcb22ba3017102a3f17954ca4b078
solomon-211 <s.leek@alustudent.com>
2025-07-31 15:17:58 -0700

    7-error_code.py

--- python-network_1/7-error_code.py
+++ python-network_1/7-error_code.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 """
-Sends a request to URL and displays response body or error code.
-Uses only requests and sys packages.
+Sends request to URL, displays response body or error code.
+Uses requests and sys packages.
 """
 
 import requests
@@ -11,6 +11,6 @@ if __name__ == "__main__":
     url = sys.argv[1]
     response = requests.get(url)
     if response.status_code >= 400:
-        print(f"Error code: {response.status_code}")
+        print("Error code: {}".format(response.status_code))
     else:
         print(response.text)
