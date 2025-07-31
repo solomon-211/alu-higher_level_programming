fe2c3dbdfb13a85bb4826454466297384cd8c91d
solomon-211 <s.leek@alustudent.com>
2025-07-31 14:57:58 -0700

    2-post_email.py

--- python-network_1/2-post_email.py
+++ python-network_1/2-post_email.py
@@ -12,6 +12,5 @@ if __name__ == "__main__":
     url = sys.argv[1]
     email = sys.argv[2]
     data = urllib.parse.urlencode({'email': email}).encode('utf-8')
-    
     with urllib.request.urlopen(url, data=data) as response:
         print(response.read().decode('utf-8'))
