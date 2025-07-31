1ecff4ab59f44817a8dbe2fe2d936ffee0acf232
solomon-211 <s.leek@alustudent.com>
2025-07-31 13:25:53 -0700

    9-cities_by_state_join.sql

---
+++ SQL_more_queries/9-cities_by_state_join.sql
@@ -0,0 +1,5 @@
+-- Lists all cities with their state names in ascending order by city ID
+SELECT cities.id, cities.name, states.name
+FROM cities, states
+WHERE cities.state_id = states.id
+ORDER BY cities.id ASC;
