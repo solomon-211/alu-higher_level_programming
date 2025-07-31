c15277c5df1f4ed4a327b4d5bdf32ca853887535
solomon-211 <s.leek@alustudent.com>
2025-07-31 15:22:03 -0700

    8-json_api.py

--- python-network_1/8-json_api.py
+++ python-network_1/8-json_api.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 """
-Sends POST request with letter parameter and processes JSON response.
-Uses requests and sys packages.
+Sends POST request with letter parameter.
+Processes JSON response from server.
 """
 
 import requests
@@ -9,12 +9,13 @@ import sys
 
 if __name__ == "__main__":
     letter = sys.argv[1] if len(sys.argv) > 1 else ""
-    response = requests.post('http://0.0.0.0:5000/search_user', data={'q': letter})
-    
+    url = 'http://0.0.0.0:5000/search_user'
+    response = requests.post(url, data={'q': letter})
     try:
         json_data = response.json()
         if json_data:
-            print("[{}] {}".format(json_data.get('id'), json_data.get('name')))
+            print("[{}] {}".format(
+                json_data.get('id'), json_data.get('name')))
         else:
             print("No result")
     except ValueError:
