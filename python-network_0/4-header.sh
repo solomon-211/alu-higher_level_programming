b15443749923db5fb2507cceab376c8d7497384d
solomon-211 <s.leek@alustudent.com>
2025-07-31 14:19:36 -0700

    4-header.sh

--- python-network_0/4-header.sh
+++ python-network_0/4-header.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# Sends GET request with required header and shows response
-curl -s -H "X-HolbertonSchool-User-Id: 98" "$1"
+# Sends GET with required header to validate route
+curl -s -X GET -H "X-HolbertonSchool-User-Id: 98" "$1" | grep -E '^OK$'
