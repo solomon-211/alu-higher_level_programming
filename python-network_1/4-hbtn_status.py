e78c3d20499b7b7c8db262f494f329dd89512f5a
solomon-211 <s.leek@alustudent.com>
2025-07-31 15:08:19 -0700

    4-hbtn_status.py

--- python-network_1/4-hbtn_status.py
+++ python-network_1/4-hbtn_status.py
@@ -1,13 +1,16 @@
 #!/usr/bin/python3
 """
-Fetches http://0.0.0.0:5050/status using requests package
-and displays the formatted response body.
+Fetches https://intranet.hbtn.io/status using requests package
+with proper error handling and displays the formatted response.
 """
 
 import requests
 
 if __name__ == "__main__":
-    response = requests.get('http://0.0.0.0:5050/status')
-    print("Body response:")
-    print("\t- type:", type(response.text))
-    print("\t- content:", response.text)
+    try:
+        response = requests.get('https://intranet.hbtn.io/status', verify=False)
+        print("Body response:")
+        print("\t- type:", type(response.text))
+        print("\t- content:", response.text)
+    except requests.exceptions.RequestException as e:
+        print("Error fetching URL:", e)
