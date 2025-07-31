5aa5681e2fcd18c8904d14cb0c03022d019068b0
solomon-211 <s.leek@alustudent.com>
2025-07-31 14:53:30 -0700

    1-hbtn_header.py

--- python-network_1/1-hbtn_header.py
+++ python-network_1/1-hbtn_header.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """
-Fetches a URL and displays the value of the X-Request-Id header from the response.
+Fetches a URL and displays the value of the X-Request-Id header.
 Uses only urllib and sys packages.
 """
 
@@ -10,5 +10,4 @@ import sys
 if __name__ == "__main__":
     url = sys.argv[1]
     with urllib.request.urlopen(url) as response:
-        x_request_id = response.getheader('X-Request-Id')
-        print(x_request_id)
+        print(response.getheader('X-Request-Id'))
