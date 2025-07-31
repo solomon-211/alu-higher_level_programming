607ee0708c639a3815c04bdc6c77adba852fd8e5
solomon-211 <s.leek@alustudent.com>
2025-07-31 14:41:56 -0700

    0-hbtn_status.py

--- python-network_1/0-hbtn_status.py
+++ python-network_1/0-hbtn_status.py
@@ -1,16 +1,20 @@
 #!/usr/bin/python3
 """
-Fetches http://0.0.0.0:5050/status using urllib package
-and displays the formatted response body.
+Fetches https://intranet.hbtn.io/status using urllib package
+with proper error handling and displays the formatted response.
 """
 
 from urllib.request import urlopen
+from urllib.error import URLError
 
 if __name__ == "__main__":
-    url = "http://0.0.0.0:5050/status"
-    with urlopen(url) as response:
-        body = response.read()
-        print("Body response:")
-        print("\t- type:", type(body))
-        print("\t- content:", body)
-        print("\t- utf8 content:", body.decode('utf-8'))
+    url = "https://intranet.hbtn.io/status"
+    try:
+        with urlopen(url) as response:
+            body = response.read()
+            print("Body response:")
+            print("\t- type:", type(body))
+            print("\t- content:", body)
+            print("\t- utf8 content:", body.decode('utf-8'))
+    except URLError as e:
+        print("Error fetching URL:", e)
