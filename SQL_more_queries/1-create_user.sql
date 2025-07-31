f511992f5714f8a06783b402c7ea4d2fa9afc927
solomon-211 <s.leek@alustudent.com>
2025-07-31 13:08:02 -0700

    1-create_user.sql

--- SQL_more_queries/1-create_user.sql
+++ SQL_more_queries/1-create_user.sql
@@ -1,9 +1,4 @@
--- Create user_0d_1 if it doesn't already exist
+-- Creates MySQL user user_0d_1 with all privileges and sets password
 CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
-
--- Grant all privileges to user_0d_1
-GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost' WITH GRANT OPTION;
-
--- Apply the changes
+GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost';
 FLUSH PRIVILEGES;
-
