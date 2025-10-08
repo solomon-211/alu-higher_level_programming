3ce1ebb738226b96b54de59c90ab4c9bb3e597bd
solomon-211 <s.leek@alustudent.com>
2025-10-07 17:15:57 -0700

    7-multi_c.js

--- javascript-warm_up/7-multi_c.js
+++ javascript-warm_up/7-multi_c.js
@@ -13,4 +13,19 @@ if (isNaN(count)) {
     i++;
   }
   console.log(output.trim());
-}
+#!/usr/bin/node
+
+const arg = process.argv[2];
+const count = parseInt(arg);
+
+if (isNaN(count)) {
+  console.log('Missing number of occurrences');
+} else if (count > 0) {
+  let i = 0;
+  let output = '';
+  while (i < count) {
+    output += 'C is fun\n';
+    i++;
+  }
+  console.log(output.trim());
+}}
